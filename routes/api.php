e4a01f2b7b1b083bd7941249f5ef3921dffc1b60
PhucLa1 <phucminhbeos@gmail.com>
2023-12-28 22:12:40 +0700

    Add role in Tags

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,15 @@ Route::post('signup',[AuthController::class,'Register']);
 Route::post('login',[AuthController::class,'Login']);
 
 //CRUD
-Route::apiResource('tag',TagController::class)->middleware('auth:api');
+//Tag
+Route::prefix('tag')->group(function(){
+    Route::get('/{project_id}',[TagController::class,'index']);
+    Route::get('/{id}',[TagController::class,'show']);
+    Route::post('/',[TagController::class,'store']);
+    Route::put('/{id}/{project_id}',[TagController::class,'update']);
+    Route::delete('/{id}/{project_id}',[TagController::class,'destroy']);
+    Route::delete('/removeAll/{project_id}',[TagController::class,'removeAll']);
+})->middleware('auth:api');
+Route::delete('removeAllTag',[TagController::class,'removeAll'])->middleware('auth:api');
 Route::apiResource('todo',ToDoController::class)->middleware('auth:api');
 Route::apiResource('project',ProjectController::class)->middleware('auth:api');

3ebb941b67a1df585d4f5b9354b3fd9968e21329
PhucLa1 <phucminhbeos@gmail.com>
2024-03-28 15:44:40 +0700

    Upload File into GG Cloud

--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Casts\TagCast;
 
 
 class ToDo extends Model
@@ -11,10 +12,11 @@ class ToDo extends Model
     use HasFactory;
     protected $table = 'todo';
     protected $fillable = [
-                            'name','explorer_id','title',
-                            'descriptions','start_date','finish_date',
-                            'priorities','tag','project_id','assgin_to'
-                        ];
-
-
+        'name', 'explorer_id', 'title',
+        'descriptions', 'start_date', 'finish_date',
+        'priorities', 'tag', 'project_id', 'assgin_to'
+    ];
+    protected $casts = [
+        'tag' => TagCast::class,
+    ];
 }
